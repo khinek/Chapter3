d5a3d244bc2ccd9038b52ae399bdabe24c120861
khinek <tom.shweko@gmail.com>
2014-09-10 08:41:42 -0400

    chapter3
    ,vkvkuvj7

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Chapter3
+{
+    class Program
+    {
+        static void Main(string[] args)
+            {
+            Console.WriteLine("value not in range");
+            string input = Console.ReadLine();
+            double hpr;
+            hpr = Convert.ToDouble(input);
+
+            if (hpr < 5.65)
+                Console.WriteLine("ERROR");
+            
+            }
+    }
+}
