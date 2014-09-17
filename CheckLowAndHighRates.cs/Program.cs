2d33007e260d969ddfc21d98b64411bc87011195
khinek <tom.shweko@gmail.com>
2014-09-16 22:48:19 -0400

    Final CH3 done
    The title fo real

---
+++ CheckLowAndHighRates.cs/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CheckLowAndHighRates.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter hourly pay rate");
+            string input = Console.ReadLine();
+            double hpr;
+            hpr = Convert.ToDouble(input);
+            if (hpr < 5.65 || hpr > 49.99)
+                Console.WriteLine("Error");
+        }
+    }
+}
---
+++ Payroll.cs/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Payroll.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter hourly pay rate");
+            double HPR = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter hours worked");
+            double HW = Convert.ToDouble(Console.ReadLine());
+
+
+            double gross = HPR * HW;
+            double witholding;
+            if (gross > 300)
+                witholding = .12;
+            else
+                witholding = .10;
+
+            double NP = gross - (gross * witholding);
+
+            Console.WriteLine("\n\nYour gross pay is $" + gross);
+            Console.WriteLine("Your witholding tax is " + (witholding * 100) + "%");
+            Console.WriteLine("Your net pay is $" + NP);
+            Console.ReadLine();
+        }
+    }
+}
